15d9a231e13165243d6cdd0599b751d57241ebe7
Jakub Smid <jakubsmid22@gmail.com>
2024-07-16 23:15:31 +0200

    Added Js for searching meals

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const recipesContainer = document.getElementById("recipe");
 const searchMealForm = document.getElementById("searchMealForm");
 const mealInput = document.getElementById("meal");
 const recipeTemplate = document.getElementById("recipeTemplate").content;
+const recipeModal = document.getElementById("recipeModal");
+const modalName = document.getElementById("modalName");
+const modalCategory = document.getElementById("modalCategory");
+const modalArea = document.getElementById("modalArea");
+const modalImg = document.getElementById("modalImg");
+const instructions = document.getElementById("instructions");
+const modalClose = document.querySelectorAll("[data-modal-close]");
 
 searchMealForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -16,12 +23,13 @@ searchMealForm.addEventListener("submit", (e) => {
   })
   .then(data => {
     data.meals.forEach(e => {
-        const {idMeal, strArea, strCategory, strMeal, strMealThumb} = e;
+        const {idMeal, strArea, strInstructions, strCategory, strMeal, strMealThumb} = e;
         const meal = recipeTemplate.cloneNode(true);
         const img = meal.getElementById("recipe-img");
         const name = meal.getElementById("name");
         const category = meal.getElementById("category");
         const area = meal.getElementById("area");
+        const btn = meal.getElementById("viewRecipe");
 
         img.src = strMealThumb;
         name.textContent = strMeal;
@@ -29,6 +37,15 @@ searchMealForm.addEventListener("submit", (e) => {
         area.textContent = strArea;
         meal.key = idMeal;
         recipesContainer.append(meal);
+
+        btn.addEventListener("click", () => {
+            modalName.textContent = strMeal;
+            modalCategory.textContent = strCategory;
+            modalArea.textContent = strArea;
+            modalImg.src = strMealThumb;
+            instructions.textContent = strInstructions;
+            recipeModal.showModal();
+        });
     })
 })
   .catch(err => {
@@ -36,3 +53,9 @@ searchMealForm.addEventListener("submit", (e) => {
   });
 
 });
+
+modalClose.forEach(e => {
+    e.addEventListener("click", () => {
+        recipeModal.close();
+    });
+})
